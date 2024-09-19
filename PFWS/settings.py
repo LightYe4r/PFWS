296843b126942b8ae235df025144c4f63685a6fb
LightYe4r <wjdakstjd@hanyang.ac.kr>
2024-09-19 18:50:53 +0900

    ADD: mysql & requirements.txt

--- PFWS/settings.py
+++ PFWS/settings.py
@@ -73,14 +73,23 @@ WSGI_APPLICATION = 'PFWS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testDB',
+        'USER': 'root',
+        'PASSWORD': 'passwd',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
